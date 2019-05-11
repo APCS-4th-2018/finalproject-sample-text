96195f086514038984a45b7e320c52b56dbfa11d
Alex <co2c6h12o6@gmail.com>
2019-05-10 17:29:10 -0700

    Classifier starter code.

---
+++ Classifier.java
@@ -0,0 +1,13 @@
+
+
+/**
+ * Classifer class to find regions of intrest in images based on haar cacades.
+ * Uses <code>OpenCV</code> vesion 4.1.0
+ *
+ * @author Alex Liu
+ * @version 1.0
+ */
+public class Classifier
+{
+
+}
